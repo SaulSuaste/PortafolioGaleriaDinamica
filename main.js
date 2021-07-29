0ac1392d6c450c818bbf5aad7fd440576c9bd985
Saul Suaste <sumsaul94@hotmail.com>
2021-07-29 18:35:56 -0500

    Busqueda por texto concluida

--- main.js
+++ main.js
@@ -14,8 +14,7 @@ window.addEventListener('load', ()=>{
   //se le agregue un estilo
   document.getElementById("grid").classList.add("imagenes-cargadas");
 
-
-  // Para la seleccion de categorias ********************************
+  // ************** Para la seleccion de categorias ********************
 
   const enlaces = document.querySelectorAll("#categorias a")
   enlaces.forEach(elemento => {
@@ -30,8 +29,7 @@ window.addEventListener('load', ()=>{
       //console.log(evento.target); //obtiene el elemento al cual se clickea
       evento.target.classList.add("activo"); //Se agrega clase activo al elemento seleccionado
 
-
-      // Filtrar por categoria **************************************
+      // ******************** Filtrar por categoria **************************
 
       //obtiene del elemento seleccionado el texto y lo convierte a minusculas, ver que los elementos lo tengan en minusculas
       const categoria = evento.target.innerHTML.toLowerCase();
@@ -41,9 +39,18 @@ window.addEventListener('load', ()=>{
       //Se ejecuta un IF(?) ELSE(:), Si categoria es igual a todos muestra los elementos que 
       //tengan la etiqueta personalizada ("[data-categoria]"), de lo contrario muestra si tienen una en especifico
       categoria === 'todos' ? grid.filter("[data-categoria]") : grid.filter(`[data-categoria='${categoria}']`); //se pasa la categoria seleccionada anteriormente
-
-
     });
   });
   
+  // ******************* Barra de Busqueda ****************************
+  //Se obtiene el elemento con id "barra-busqueda", se agrega evento para que al ingresar un texto
+  //ejecute la funcion
+  document.getElementById("barra-busqueda").addEventListener('input', evento => {
+    const busqueda = evento.target.value.toLowerCase(); //obtiene el valor ingresado en el cuadro de texto
+    console.log(busqueda);
+    //Al modulo filter se le pasa una funcion flecha que obtiene la etiqueta personalizada "data-etiquetas"
+    //que contenga lo que el usuario este ingresando en el cuadro de texto
+    grid.filter(img => img.getElement().dataset.etiquetas.includes(busqueda));
+  })
+
 });
